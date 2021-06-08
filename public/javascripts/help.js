0c0e484dbc23dbdb62984953a9de382503cd829e
WangSihan1118 <wangsihan1118@gmail.com>
2021-06-08 03:07:52 +0000

    basic login

---
+++ public/javascripts/help.js
@@ -0,0 +1,80 @@
+function getCheckedValue(name){
+    var val_arr = document.getElementsByName(name);
+    var checked_val;
+    for(var i = 0; i < val_arr.length; i++){
+        if(val_arr[i].checked){
+            checked_val = val_arr[i].value;
+        }
+    }
+    return checked_val;
+}
+
+function login(){
+    var ID = document.getElementById("ID").value;
+    var pwd = document.getElementById("pwd").value;
+    var login_type = getCheckedValue("AccountType");
+    
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            if(this.responseText){
+                window.location.href="account.html";
+            }else{
+                alter("Failed");
+            }
+            return;
+        }
+    };
+    xhttp.open("POST", "/login", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify({ 
+        ID:ID,
+        pwd:pwd,
+        login_type:login_type
+    }));
+}
+
+function register(){
+    var number = document.getElementById("Number").value;
+    var pwd = document.getElementById("pwd").value;
+    var dpwd = document.getElementById("dpwd").value;
+    var r_type = getCheckedValue("AccountType");
+    
+    if(pwd!=dpwd){
+        alert("Please check your password!");
+        return;
+    }
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            if(this.responseText){
+                var a_id = this.responseText;
+                alert("Your ID is " + a_id);
+                window.location.href="login.html";
+            }else{
+                alter("Failed");
+            }
+            return;
+        }
+    };
+    xhttp.open("POST", "/register", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify({ 
+        number:number,
+        pwd:pwd,
+        r_type:r_type
+    }));
+}
+
+function checkin(){
+    var ck = document.getElementById("checkin_input").value;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/checkin", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify({ 
+        ck:ck
+    }));
+}
--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,104 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+router.post('/login',function(req,res,next){
+    req.pool.getConnection( function(err,connection) {
+        //var user_id = req.session.user_id;
+        var ID = req.body.ID;
+        var pwd = req.body.pwd;
+        var login_type= req.body.login_type;
+        var query;
+        if(login_type == "User"){
+            login_type = "user";
+            query = "select * from user where ID = ? and password = ?";
+        }else if(login_type == "Manager"){
+            login_type = "manager";
+            query = "select * from manager where ID = ? and password = ?";
+        }else if(login_type == "Admin"){
+            login_type = "admin";
+            query = "select * from admin where ID = ? and password = ?";
+        }
+        var sql_params = new Array();
+        sql_params.push(ID);
+        sql_params.push(pwd);
+        
+
+        if (err) {
+            res.sendStatus(500);
+            return;
+        }
+        
+        connection.query(query, sql_params,function(err, rows, fields) {
+            connection.release(); // release connection
+            if (err) {
+                res.sendStatus(500);
+                return;
+            }
+            if(rows){
+                req.session.type = login_type;
+                req.session.ID = ID;
+                res.send(true);
+            }else{
+                res.send(false);
+            }
+        });
+    });
+});
+
+router.post('/register',function(req,res,next){
+    req.pool.getConnection( function(err,connection) {
+        //var user_id = req.session.user_id;
+        var ID = req.body.number;
+        var pwd = req.body.pwd;
+        var r_type= req.body.r_type;
+
+        var sql_params = new Array();
+        sql_params.push(first_name);
+        sql_params.push(last_name);
+        sql_params.push(number);
+        sql_params.push(Email);
+        sql_params.push(address);
+        sql_params.push(health);
+        //sql_params.push(user_id);
+
+        if (err) {
+            res.sendStatus(500);
+            return;
+        }
+        var query = "UPDATE user SET first_name= ?, last_name = ?,phone_number = ?, email = ?,address = ?, health = ? where ID = ?";
+        connection.query(query, sql_params,function(err, rows, fields) {
+            connection.release(); // release connection
+            if (err) {
+                res.sendStatus(500);
+                return;
+            }
+            res.json(rows);
+        });
+    });
+});
+
+router.post('/checkin',function(req,res,next){
+    req.pool.getConnection( function(err,connection) {
+        //var user_id = req.session.user_id;
+        var login_type= req.body.ck;
+
+        var sql_params = new Array();
+        sql_params.push(first_name);
+        //sql_params.push(user_id);
+
+        if (err) {
+            res.sendStatus(500);
+            return;
+        }
+        var query = "UPDATE user SET first_name= ?, last_name = ?,phone_number = ?, email = ?,address = ?, health = ? where ID = ?";
+        connection.query(query, sql_params,function(err, rows, fields) {
+            connection.release(); // release connection
+            if (err) {
+                res.sendStatus(500);
+                return;
+            }
+            res.json(rows);
+        });
+    });
+});
